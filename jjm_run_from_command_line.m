a4d46da16dd4799053827501a3bbebb00a568db9
marburyvthesea <jjm2128@gmail.com>
2019-03-28 20:19:33 -0500

    new stuff

---
+++ jjm_run_from_command_line.m
@@ -0,0 +1,16 @@
+% path to tiff or hdf5 file
+
+file_to_analyze = '?/projects/p30771/miniscope/data/GRIN011/1_24_2019/H10_M19_S59/TIFs/caiman_analysis' ;
+
+% add file to path and CNMFE repositories
+
+addpath(file_to_analyze) ;
+addpath(genpath('/projects/p30771/MATLAB/CNMF_E')) ;
+
+% run script with options
+
+cd('/projects/p30771/MATLAB/CNMF_E/quest_analysis/quest_MATLAB_cnmfe');
+
+run_cnmfe_matlab_jjm(file_to_analyze, 13, 40, 20, false) ;
+
+disp('finished cnmfe run') 
