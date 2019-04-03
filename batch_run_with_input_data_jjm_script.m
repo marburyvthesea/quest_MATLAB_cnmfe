5d2618d1dd087108c300ef127009dbc953a47465
marburyvthesea <jjm2128@gmail.com>
2019-04-03 15:06:39 -0500

    changes

--- batch_run_with_input_data_jjm_script.m
+++ batch_run_with_input_data_jjm_script.m
@@ -8,18 +8,28 @@ disp(folder_path)
 %combine paths into cell array
 
 files = {dir(fullfile(folder_path, '*.h5'))} ;
-size_ = [size(files)] ; 
-len = size_(1) ; 
+size_ = [size(files{1,1})] ; 
+num_files = size_(1) ; 
 file_names = {files{1,1}.name} ;
 
+disp(num_files)
+
 files_array = {} ;
-for i = 1:len 
-  files_array{i,1} = files_names{1,i} ; 
+for i = 1:num_files 
+	disp(i)
+	files_array{i,1} = file_names{1,i} ; 
 end
 
 nams = files_array ; 
 disp('found files')
 disp(nams)
+addpath(folder_path)
+
+
+addpath('/projects/p30771/MATLAB/CNMF_E/') ;
+cnmfe_setup ; 
+
+disp('running cnmfe')
 
 call batch_run.m to start batch CNMF_E
 run_cnmfe_batch_matlab_jjm(nams, 13, 40, 20, false)
