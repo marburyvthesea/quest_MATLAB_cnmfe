32f113acb0843b004fd41bc1143590dec49ec076
chief <chief>
2019-09-26 23:51:04 -0400

    changes

--- slurm_quest_CNMFE_matlab_submission_batch_array.sh
+++ slurm_quest_CNMFE_matlab_submission_batch_array.sh
@@ -11,40 +11,42 @@
 #SBATCH --job-name="slurm_matlab_cnmfe_run_batch_array"
 
 module purge all
-module load anaconda3/2018.12
+#module load anaconda3/2018.12
 
 cd ~
 
 #path to file (h5 or tiff should work)
 
-#FILE_path=$1
+FILE_path=$1
 
 # other parameters for CNMF_E
+#if want to input different variables for job use ${SLURM_ARRAY_TASK_ID}p variable to get from input.csv file
 
-#INPUT_gSig=7
-#INPUT_gSiz=21
-#INPUT_Fs=20
+echo "running job ${SLURM_ARRAY_TASK_ID} with input variable"
+echo $(sed -n ${SLURM_ARRAY_TASK_ID}p input.csv)
+
+INPUT_gSig=$(sed -n ${SLURM_ARRAY_TASK_ID}p input.csv)
+INPUT_gSiz=21
+INPUT_Fs=20
 #spatial downsampling 
-#INPUT_ssub=2
+INPUT_ssub=2
 #use parallel processing
-#INPUT_parallel_enable=false
+INPUT_parallel_enable=false
 
-#add project directory to PATH
-#export PATH=$PATH/projects/p30771/
-
-#load modules to use
-#module load python/anaconda3.6 
+add project directory to PATH
+export PATH=$PATH/projects/p30771/
 
 #load modules to use
-#module load matlab/r2018a
+module load python/anaconda3.6 
+
+#load modules to use
+module load matlab/r2018a
 
 #cd to script directory
 cd /projects/p30771/MATLAB/CNMF_E_jjm/quest_MATLAB_cnmfe
  
 # run the script 
-echo $(sed -n ${SLURM_ARRAY_TASK_ID}p input.csv)
 
-#python batcharrayscript.py $(sed -n ${SLURM_ARRAY_TASK_ID}p input.csv | sed 's/,/;/g')
 #matlab -nosplash -nodesktop -r "file_to_analyze='$FILE_path';gSig='$INPUT_gSig';gSiz='$INPUT_gSiz';Fs='$INPUT_Fs';ssub='$INPUT_ssub';parallel_enable='$INPUT_parallel_enable';disp(file_to_analyze);run('/projects/p30771/MATLAB/CNMF_E_jjm/quest_MATLAB_cnmfe/jjm_run_from_command_line.m');exit;"
 
 
