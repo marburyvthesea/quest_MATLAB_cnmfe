b6e0d9098aba422e50faf6aa2338b809eafd51ea
marburyvthesea <jjm2128@gmail.com>
2019-10-10 11:15:42 -0500

    changes

--- slurm_quest_CNMFE_matlab_submission.sh
+++ slurm_quest_CNMFE_matlab_submission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p30771
-#SBATCH -p short
-#SBATCH -t 0:05:00
+#SBATCH -p short 
+#SBATCH -t 2:00:00
 #SBATCH -o ./logfiles/slurm.%x-%j.out # STDOUT
 #SBATCH --job-name="slurm_matlab_cnmfe_run"
 #SBATCH --mem=90G
