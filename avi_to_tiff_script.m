2b6000f74e423efd611cc0f811179b7c00dd29d9
marburyvthesea <jjm2128@gmail.com>
2019-10-10 13:24:48 -0500

    changes

---
+++ avi_to_tiff_script.m
@@ -0,0 +1,15 @@
+
+
+
+
+
+% add file to path and CNMFE repositories
+
+addpath(dirpath) ;
+addpath(genpath('/projects/p30771/MATLAB/CNMF_E_jjm/')) ;
+
+% run script with options
+
+cd('/projects/p30771/MATLAB/CNMF_E_jjm/quest_MATLAB_cnmfe/');
+
+fn_directory_to_multitiff(dirpath, start, end, parallel) ;
---
+++ slurm_quest_avi_to_tiff.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH -A p30771
+#SBATCH -p short 
+#SBATCH -t 2:00:00
+#SBATCH -o ./logfiles/slurm.%x-%j.out # STDOUT
+#SBATCH --job-name="slurm_matlab_avi_to_tiff"
+#SBATCH --mem=90G
+#SBATCH -N 1
+#SBATCH -n 20 
+
+
+module purge all
+
+cd ~
+
+# Inputs from command line
+DIR_path=$1
+
+# 
+
+INPUT_moviestart=$2
+INPUT_movieend=$3
+INPUT_parallel_enable=$4
+
+#add project directory to PATH
+export PATH=$PATH/projects/p30771/
+
+#load modules to use
+
+module load matlab/r2018a
+
+#cd to script directory
+cd /projects/p30771/MATLAB/CNMF_E_jjm/quest_MATLAB_cnmfe
+#run  
+
+matlab -nosplash -nodesktop -r "dirpath='$DIR_path';start='$INPUT_moviestart';end='$INPUT_movieend';parallel='$INPUT_parallel_enable';disp(dirpath);run('/projects/p30771/MATLAB/CNMF_E_jjm/quest_MATLAB_cnmfe/avi_to_tiff_script.m');exit;"
+
+
